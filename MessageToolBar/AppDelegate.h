a07963050c5426c9ea613eb236d5de89d8b1fe26
zhanghaidi <haidi.zhang@quantgroup.cn>
2017-04-19 18:12:12 +0800

    Initial Commit

---
+++ MessageToolBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageToolBar
+//
+//  Created by zhanghaidi on 2017/4/19.
+//  Copyright © 2017年 zhanghaidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageToolBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MessageToolBar
+//
+//  Created by zhanghaidi on 2017/4/19.
+//  Copyright © 2017年 zhanghaidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
